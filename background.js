32b5d301d0542382515f09fecfd883b9ed488468
kimlongg <nguyenkimlong1205@gmail.com>
2023-03-25 00:13:08 +0700

    asd21

--- background.js
+++ background.js
@@ -9,38 +9,30 @@ chrome.runtime.onInstalled.addListener(() => {
     chrome.contextMenus.create({
         id: "myContextMenu",
         title: "Chọn tôi",
-        contexts: ["link", "image"],
-        onclick: function (info, tab) {
+        contexts: ["link", "image"]
+    });
+});
+
+chrome.contextMenus.onClicked.addListener(function (info, tab) {
+    if (info.menuItemId === "myContextMenu") {
+        // Lấy thông tin về đường dẫn của trang showme.html
+        const url = chrome.runtime.getURL("showme.html");
+
+        // Tạo một tab mới và chuyển đến trang HTML của tiện ích mở rộng
+        chrome.tabs.create({ url: url }, function (tab) {
+            // Kiểm tra xem đối tượng chrome.storage đã được khởi tạo chưa
+            if (chrome.runtime.lastError) {
+                console.error(chrome.runtime.lastError);
+                return;
+            }
+
             // Lưu thông tin về liên kết hoặc hình ảnh mà người dùng đã nhấp vào vào chrome.storage.local
             chrome.storage.local.set({ clickedItem: info });
-
-            // Tạo một tab mới và chuyển đến trang HTML của tiện ích mở rộng
-            chrome.tabs.create({ url: chrome.runtime.getURL("showme.html") });
-        }
-    });
-});
-
-
-
-async function handleContextMenuClick(info, tab) {
-    const url = info.srcUrl || info.linkUrl;
-    const type = info.srcUrl ? 'image' : 'link';
-
-    // Chèn content_script.js vào trang web hiện tại với các biến url và type
-    await chrome.scripting.executeScript({
-        target: { tabId: tab.id },
-        files: ['content_script.js']
-    });
-
-    // Gửi thông điệp đến content_script.js với url và type
-    chrome.tabs.sendMessage(tab.id, { url, type });
-}
-
-chrome.contextMenus.onClicked.addListener((info, tab) => {
-    if (info.menuItemId === 'myContextMenu') {
-        handleContextMenuClick(info, tab);
+        });
     }
 });
+
+
 
 
 
